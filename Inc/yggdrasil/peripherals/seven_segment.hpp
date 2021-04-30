813d9d2304b7ff2769394b2590fe7e38b98cd6b9
Fabian <webef4@bfh.ch>
2021-04-30 08:44:58 +0200

    yggdrasil: Fixed signed and float, added comments

--- Inc/yggdrasil/peripherals/seven_segment.hpp
+++ Inc/yggdrasil/peripherals/seven_segment.hpp
@@ -11,7 +11,7 @@
   *  @ingroup Peripherals                                           *
   *  @author Fabian Weber, Nikolaij Saegesser						*
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  *  @brief API to use the 7 segment display 	 	        		*
+  *  @brief Driver to use the 7 segment display 	 	       		*
   *  			                                                    *
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   * This software can be used by students and other personal of the *
@@ -40,73 +40,73 @@ namespace bsp::ygg::prph {
 	public:
 		SevenSegment() = delete;
 
-		enum class Mode {
-			Unsigned,
-			Signed,
-			Hexdecimal,
-			FloatingPoint,
-
-			DigitA,
-			DigitB,
-			DigitC,
-			DigitD,
-		};
-
 		/**
-		 * @brief
+		 * @brief display a unsigned value from 0 to 9999 on the 7 segments
+		 * @note the function takes care of the multiplexing, but must be called frequently (>= 200Hz recommended)
 		 *
-		 * @return
-		 * @note
+		 * @param unsigned value to display
 		 */
 		static void setUnsigned(u16 value) {
 			static u8 currSegment = 0;
 
-			enableDigit(currSegment);
+			enableDigit(currSegment);	// Enable the current segment
 
 			switch(currSegment){
 				case 0:
-					SevenSegment::setDigit((value / 1000) % 10);
+					SevenSegment::setDigit((value / 1000) % 10);	// Display thousands
 				break;
 				case 1:
-					SevenSegment::setDigit((value / 100) % 10);
+					SevenSegment::setDigit((value / 100) % 10);		// Display hundreds
 				break;
 				case 2:
-					SevenSegment::setDigit((value / 10) % 10);
+					SevenSegment::setDigit((value / 10) % 10);		// Display tens
 				break;
 				case 3:
-					SevenSegment::setDigit((value / 1) % 10);
+					SevenSegment::setDigit((value / 1) % 10);		// Display ones
 				break;
 			}
-			currSegment = (currSegment + 1) % 4;
+			currSegment = (currSegment + 1) % 4;	// Increment current segment
 		}
 
+		/**
+		 * @brief display a signed value from -999 to 999 on the 7 segments
+		 * @note the function takes care of the multiplexing, but must be called frequently (>= 200Hz recommended)
+		 *
+		 * @param signed value to display
+		 */
 		static void setSigned(i16 value) {
 			static u8 currSegment = 0;
 
-			enableDigit(currSegment);
+			enableDigit(currSegment);	// Enable the current segment
 
 			switch(currSegment){
 				case 0:
-					if(value < 0) SevenSegment::setDigit('-');
+					if(value < 0) SevenSegment::setDigit('-');				// Display the minus
 					else SevenSegment::setDigit(' ');
 				break;
 				case 1:
-					SevenSegment::setDigit((std::abs(value) / 100) % 10);
+					SevenSegment::setDigit((std::abs(value) / 100) % 10);	// Display hundreds
 				break;
 				case 2:
-					SevenSegment::setDigit((std::abs(value) / 10) % 10);
+					SevenSegment::setDigit((std::abs(value) / 10) % 10);	// Display tens
 				break;
 				case 3:
-					SevenSegment::setDigit((std::abs(value) / 1) % 10);
+					SevenSegment::setDigit((std::abs(value) / 1) % 10);		// Display ones
 				break;
 			}
-			currSegment = (currSegment + 1) % 4;
+			currSegment = (currSegment + 1) % 4;	// Increment current segment
 		}
 
+		/**
+		 * @brief display a unsigned value from 0 to 0xFFFF in hexadecimal on the 7 segments
+		 * @note the function takes care of the multiplexing, but must be called frequently (>= 200Hz recommended)
+		 *
+		 * @param unsigned value to display in hexadecimal
+		 */
 		static void setHexadecimal(u16 value) {
 			static u8 currSegment = 0;
 
-			enableDigit(currSegment);
+			enableDigit(currSegment); // Enable the current segment
 
 			switch(currSegment){
 				case 0:
@@ -122,33 +122,55 @@ namespace bsp::ygg::prph {
 					SevenSegment::setDigit((value / 0x1) % 0x10);
 				break;
 			}
-			currSegment = (currSegment + 1) % 4;
+			currSegment = (currSegment + 1) % 4;	// Increment current segment
 		}
 
+		/**
+		 * @brief display a floating point value from -99.9 to 99.9 on the 7 segments, the function will show as many decimal places as possible
+		 * @note the function takes care of the multiplexing, but must be called frequently (>= 200Hz recommended)
+		 *
+		 * @param float value to display
+		 */
 		static void setFloatingPoint(float value) {
 			static u8 currSegment = 0;
 
 			enableDigit(currSegment);
 			switch(currSegment){
 				case 0:
-					if(value < 0) SevenSegment::setDigit('-');
+					if(value < 0) SevenSegment::setDigit('-');														// Display the minus
+					else SevenSegment::setDigit(' ');
 				break;
 				case 1:
-					SevenSegment::setDigit(static_cast<u8>(value / 10) % 10);
+					if(std::abs(value) >= 10) SevenSegment::setDigit(static_cast<u8>(std::abs(value) / 10) % 10);	// Display tens
+					else {
+						SevenSegment::setDigit(static_cast<u8>(std::abs(value) / 1) % 10);							// Display ones
+						seven_segment::DP = true;
+					}
 				break;
 				case 2:
-					SevenSegment::setDigit(static_cast<u8>(value / 1) % 10);
-					seven_segment::DP = false;
+					if(std::abs(value) >= 10) {
+						SevenSegment::setDigit(static_cast<u8>(std::abs(value) / 1) % 10);							// Display ones
+						seven_segment::DP = true;
+					}
+					else SevenSegment::setDigit(static_cast<u8>(std::abs(value) * 10) % 10);						// Display tenth
+
 				break;
 				case 3:
-					SevenSegment::setDigit(static_cast<u8>(value * 10) % 10);
+					if(std::abs(value) >= 10) SevenSegment::setDigit(static_cast<u8>(std::abs(value) * 10) % 10);	// Display tenth
+					else SevenSegment::setDigit(static_cast<u8>(std::abs(value) * 100) % 10);						// Display hundredth
 				break;
 			}
 
-			currSegment = (currSegment + 1) % 4;
+			currSegment = (currSegment + 1) % 4;	// Increment current segment
 
 		}
 
+		/**
+		 * @brief set the segments to the given value, supports hexadecimal, '-' for minus and ' ' for an empty segment
+		 * @note the function does not enable any segment
+		 *
+		 * @param value to enable the segments
+		 */
 		static void setDigit(char value){
 			switch(value){
 				case 0:		seven_segment::All = 0x3F; break;
@@ -173,6 +195,11 @@ namespace bsp::ygg::prph {
 
 		}
 
+		/**
+		 * @brief enable one single segment
+		 *
+		 * @param number to enable a single segment
+		 */
 		static void enableDigit(u8 number){
 			switch(number){
 				case 0:		seven_segment::SelectAll = 0x01; break;
