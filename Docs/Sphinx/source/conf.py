e8ed41e20ec3d7290a7759ca8aa089f5ecc92a08
WerWolv <werwolv98@gmail.com>
2021-05-27 13:30:52 +0200

    docs: Removed "Built with Sphinx" and "Show source" footer text

--- Docs/Sphinx/source/conf.py
+++ Docs/Sphinx/source/conf.py
@@ -27,6 +27,9 @@ release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+html_show_sourcelink = False
+html_show_sphinx = False
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
